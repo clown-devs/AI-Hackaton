6eaa978c114baee4cabeba4d4ea2d769a75a5616
Simoffsky <maks-kirsanov26@mail.ru>
2024-05-31 20:47:57 +1000

    added args

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 import parser.parser as parser
+import sys
 
-data,header = parser.parse_file('N-1.edf')
-parser.save_to_csv(data, header, 'N-1.csv')
+
+if len(sys.argv) != 3:
+    print("Usage: python main.py <input_file> <output_file>")
+    sys.exit(1)
+
+
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
+
+data, header = parser.parse_file(input_file)
+parser.save_to_csv(data, header, output_file)
