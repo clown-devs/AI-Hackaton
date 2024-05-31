2cfda1062f9c7e8c92ef0e27d8dcd72e3d03f0a9
Simoffsky <maks-kirsanov26@mail.ru>
2024-06-01 00:56:25 +1000

    added n query parameter

--- server/main.py
+++ server/main.py
@@ -14,17 +14,19 @@ class Item(BaseModel):
 
 # gets file in .edf format and returns list of episodes with their types
 @app.post("/")
-async def read_rec(file: UploadFile):
+async def read_rec(file: UploadFile, n: int = 10):
     contents = await file.read()
     hash_data = hashlib.md5(contents).hexdigest()
     hex_dig = hashlib.md5(contents).hexdigest()
     with open(f"{hex_dig}.edf", "wb") as f:
         f.write(contents)
     
-    return [
-        Item(episode=1, start=0, end=10, duration=10, type="Апноэ"),
-        Item(episode=2, start=40, end=50, duration=10, type="Гиперопноэ"),
-    ]
+    return generate_random_array(n)
     
 
-
+def generate_random_array(n: int) -> List[Item]:
+    import random
+    arr = []
+    for i in range(n):
+        arr.append(Item(episode=i, start=random.randint(0, 100), end=random.randint(0, 100), duration=random.randint(0, 100), type="random"))
+    return arr
