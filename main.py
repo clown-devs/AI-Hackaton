421579705955ae01c33aa784418747595905d678
Simoffsky <maks-kirsanov26@mail.ru>
2024-05-31 20:08:29 +1000

    added parser

--- main.py
+++ main.py
@@ -1,31 +1,4 @@
-# import numpy as np
-# import mne
+import parser.parser as parser
 
-# edf = mne.io.read_raw_edf('N-1.edf')
-# header = ','.join(edf.ch_names)
-
-# data = edf.get_data().T
-
-# np.savetxt('test.csv', data, delimiter=',', header=header)
-
-import numpy as np
-import mne
-
-edf = mne.io.read_raw_edf('N16-1.edf')
-header = edf.ch_names[0]  
-print(edf.info)
-
-data = edf.get_data()[0]  
-print(len(data))
-data = data.reshape(-1, 1) 
-#np.savetxt('test.csv', data, delimiter=',', header=header)
-
-# import matplotlib.pyplot as plt
-
-
-# plt.figure(figsize=(10, 5))
-# plt.plot(data)
-# plt.title('Data from the first channel')
-# plt.xlabel('Time')
-# plt.ylabel('Amplitude')
-# plt.show()
+data,header = parser.parse_file('N-1.edf')
+parser.save_to_csv(data, header, 'N-1.csv')
---
+++ parser/parser.py
@@ -0,0 +1,20 @@
+import numpy as np
+import mne
+from typing import IO
+from typing import Tuple
+
+def parse_file(file_path: str) -> Tuple[np.ndarray, str]:
+    edf = mne.io.read_raw_edf(file_path)
+    header = ','.join(edf.ch_names[:6]) # Хардкодим первые 6 каналов
+
+    data = edf.get_data()[:6].T # Хардкодим первые 6 каналов
+
+    return data, header
+
+
+def save_to_csv(data: np.ndarray, header: str, output_filename: str):
+    np.savetxt(output_filename, data, delimiter=',', header=header)
+
+data, header = parse_file('N-1.edf')
+
+save_to_csv(data, header, 'N-1.csv')
