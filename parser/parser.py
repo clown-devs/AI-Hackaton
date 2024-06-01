f7c9245df96020608ce40cb2d10a70facc4e1578
Simoffsky <maks-kirsanov26@mail.ru>
2024-06-01 17:17:59 +1000

    fixed parser

--- parser/parser.py
+++ parser/parser.py
@@ -5,12 +5,12 @@ from typing import Tuple
 
 def parse_file(file_path: str) -> Tuple[np.ndarray, str]:
     edf = mne.io.read_raw_edf(file_path)
-    header = ','.join(edf.ch_names[:6]) # Хардкодим первые 6 каналов
 
     data = edf.get_data()[:6].T # Хардкодим первые 6 каналов
 
-    return data, header
+    return data, edf
 
 
-def save_to_csv(data: np.ndarray, header: str, output_filename: str):
+def save_to_csv(data: np.ndarray, edf, output_filename: str):
+    header = ','.join(edf.ch_names[:6])
     np.savetxt(output_filename, data, delimiter=',', header=header)
