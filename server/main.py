861b54aa1244c6a5b9b571325597b00e17efa777
Simoffsky <maks-kirsanov26@mail.ru>
2024-06-02 10:24:47 +1000

    added disease types

--- server/main.py
+++ server/main.py
@@ -33,9 +33,10 @@ async def read_rec(file: UploadFile, n: int = 10):
     
 
 def generate_random_array(n: int) :
+    types = ["апное","гипопноэ", "центральное апноэ", "обструктивное апноэ", "???", "храп"]
     import random
     
     data = ItemList(items=[], word_url="static/example.docx", dead=random.choice([True, False]))
     for i in range(n):
-        data.items.append(Item(episode=i, start=random.randint(0, 100), end=random.randint(0, 100), duration=random.randint(0, 100), type="type"))
+        data.items.append(Item(episode=i, start=random.randint(0, 100), end=random.randint(0, 100), duration=random.randint(0, 100), type=random.choice(types)))
     return data
